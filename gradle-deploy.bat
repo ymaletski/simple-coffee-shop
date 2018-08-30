2332229869af164a0066eb28571b3ccfbb206f5c
ymaletski <yura.maletski@mail.ru>
2018-08-30 08:20:53 +0300

    frame of the project

---
+++ gradle-deploy.bat
@@ -0,0 +1,3 @@
+call gradle clean
+call gradle build
+call java -jar build/libs/simple-coffee-shop-0.1.0.jar
---
+++ src/main/java/by/yura/coffee_shop/simplecoffeeshop/SimpleCoffeeShopApplication.java
@@ -0,0 +1,12 @@
+package by.yura.coffee_shop.simplecoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCoffeeShopApplication.class, args);
+	}
+}
